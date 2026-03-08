50ac76441544ee32c8245d0b7c2800fe9975ae03
abhidhumal0102 <125811468+abhidhumal0102@users.noreply.github.com>
2026-03-09 02:09:19 +0530

    Add theme installation script for WhiteSur themes

---
+++ ci/airootfs/root/theme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /opt
+
+git clone https://github.com/vinceliuice/WhiteSur-kde
+cd WhiteSur-kde
+./install.sh
+
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme
+cd WhiteSur-icon-theme
+./install.sh
